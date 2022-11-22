9f449a79fab39f89cad97c62a014d513d80686f2
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-11-22 13:13:10 +0100

    javascript

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const slider = document.getElementById("slider");
 const output = document.getElementById("js-pageviews");
-output.innerText = slider.value;
+const toggle = document.querySelector(".top__billing__toggle");
+const toggleSwitch = document.querySelector(".top__billing__toggle-switch");
+const price = document.getElementById("price");
+const togglePrice = document.getElementById("month");
 
 const min = slider.min;
 const max = slider.max;
 const value = slider.value;
+
+output.innerText = slider.value;
 
 slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%), hsl(174, 77%, 80%) ${
     ((value - min) / (max - min)) * 100
@@ -17,4 +22,47 @@ slider.oninput = function () {
         ((this.value - this.min) / (this.max - this.min)) * 100
     }%, #DEE2E6 100%)`;
     output.innerHTML = this.value;
+
+    checkPrice();
 };
+
+function checkPrice() {
+    checkYearly();
+}
+
+function checkMonthly() {
+    let cheap = "$" + 12 + ".00";
+    let medium = "$" + 16 + ".00";
+    let expensive = "$" + 24 + ".00";
+    if (slider.value < 50) {
+        price.innerText = cheap;
+    } else if (slider.value >= 50 && slider.value < 150) {
+        price.innerText = medium;
+    } else if (slider.value >= 150) {
+        price.innerText = expensive;
+    }
+}
+
+function checkYearly() {
+    let cheap = "$" + 12 * 12 * 0.75 + ".00";
+    let medium = "$" + 16 * 12 * 0.75 + ".00";
+    let expensive = "$" + 24 * 12 * 0.75 + ".00";
+    if (slider.value < 50) {
+        price.innerText = cheap;
+    } else if (slider.value >= 50 && slider.value < 150) {
+        price.innerText = medium;
+    } else if (slider.value >= 150) {
+        price.innerText = expensive;
+    }
+}
+
+function toggletime() {
+    toggleSwitch.classList.toggle("active");
+    if ((toggleSwitch.classlist = "active")) {
+        togglePrice.innerText = "/year";
+    } else {
+        togglePrice.innerText = "/month";
+    }
+}
+
+toggle.addEventListener("click", toggletime);
